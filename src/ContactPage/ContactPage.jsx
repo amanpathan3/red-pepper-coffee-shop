99d3d1b06d8349e40a4993608c9140ba6b4c9273
Aman Pathan <amanpathan3318@gmail.com>
2025-10-19 19:15:55 +0530

    Added Contact page with header and footer

--- src/ContactPage/ContactPage.jsx
+++ src/ContactPage/ContactPage.jsx
@@ -1,25 +1,37 @@
+import {Header} from "../Components/Header";
+import {Footer} from "../Components/Footer";
 import "./ContactPage.css";
 
 export function ContactPage() {
-    return (
-        <>
-            <div className="contact-page">
-                <div className="contact-page-header">
-                    <h1>Red Pepper</h1>
-                    <p>Where every sip feels like home ☕</p>
-                </div>
-                <div className="input-form">
-                    <label htmlFor="name">Name: </label>
-                    <input type="text" placeholder="Enter your name" />
-                    <label htmlFor="mobile">Mobile No: </label>
-                    <input type="number" placeholder="Enter your mobile number" />
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" placeholder="Enter your email" />
-                    <label htmlFor="message">Message: </label>
-                    <textarea name="message" id="message"></textarea>
-                    <button>Send Message</button>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <div className="contact-page">
+        <div className="contact-container">
+          <div className="contact-header">
+            <h1>☕ Red Pepper Cafe</h1>
+            <p>Where every sip feels like home</p>
+          </div>
+
+          <div className="contact-form">
+            <label htmlFor="name">Name</label>
+            <input type="text" placeholder="Enter your name" />
+
+            <label htmlFor="mobile">Mobile No</label>
+            <input type="number" placeholder="Enter your mobile number" />
+
+            <label htmlFor="email">Email</label>
+            <input type="email" placeholder="Enter your email" />
+
+            <label htmlFor="message">Message</label>
+            <textarea placeholder="Write your message here..."></textarea>
+
+            <button>Send Message</button>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
