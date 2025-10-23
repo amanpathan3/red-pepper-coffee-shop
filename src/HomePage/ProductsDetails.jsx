1b96cb6af7808656f8099e56f9c175b3a99c0043
Aman Pathan <amanpathan3318@gmail.com>
2025-10-23 14:52:54 +0530

    fetch data from backend to displaying the single clickable cart

--- src/HomePage/ProductsDetails.jsx
+++ src/HomePage/ProductsDetails.jsx
@@ -1,25 +1,40 @@
+import { useState , useEffect} from "react";
 import { Footer } from "../Components/Footer";
 import { Header } from "../Components/Header";
-import { products } from "./products";
+
 import { useParams, Link } from "react-router-dom";
-export function ProductDetails(){
-    const {id} = useParams();
+export function ProductDetails() {
+
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const response = await fetch("http://localhost:5000/products");
+        const data = await response.json();
+        setProducts(data);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+
+    const { id } = useParams();
     const product = products.find((p) => p.id === Number(id));
 
-    if(!product){
-        return(<h2>Not</h2>);
+    if (!product) {
+        return (<h2>Not</h2>);
     }
-    return(
+    return (
         <>
             <Header />
             <div className="product-container">
                 <div key={product.id} className="product-card">
-                <img src={product.image} alt={product.name} className="product-img" />
-                <h3 className="product-name">{product.name}</h3>
-                <p className="rating">⭐ {product.rating}</p>
-                <p className="quantity">Quantity: {product.quantity}</p>
-                <p className="price">₹{product.price}</p>
-                <button className="product-btn">Buy</button>
+                    <img src={product.image} alt={product.name} className="product-img" />
+                    <h3 className="product-name">{product.name}</h3>
+                    <p className="rating">⭐ {product.rating}</p>
+                    <p className="quantity">Quantity: {product.quantity}</p>
+                    <p className="price">₹{product.price}</p>
+                    <button className="product-btn">Buy</button>
                 </div>
             </div>
             <Footer />
